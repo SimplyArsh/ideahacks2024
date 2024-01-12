4c8e0ce57609ccff2f3ccb84f58f6aa70b1837a5
Simplyarsh <arshnoorfly@gmail.com>
2024-01-11 23:56:31 -0800

    Backend template

---
+++ backend/server.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const app = express()
+
+app.get('/', (req, res) => {
+    res.json({mssg: "Welcome!"})
+})
+
+app.listen(4000, () => {
+    console.log('Listening on port 4000!')
+})
+
