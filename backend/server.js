465200491e17e03623481c55af51ed495671a674
Simplyarsh <arshnoorfly@gmail.com>
2024-01-12 00:37:55 -0800

    Backend additions

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,27 @@
+require('dotenv').config()
+
 const express = require('express')
 const app = express()
+const mongoose = require('mongoose')
+const recipe = require('./routes/recipe')
 
-app.get('/', (req, res) => {
-    res.json({mssg: "Welcome!"})
+app.use((req, res, next) => {
+    next()
 })
 
-app.listen(4000, () => {
+app.use('/api/recipe', recipe)
+
+mongoose.connect(process.env.MONGO_UI)
+.then(() => {
+    app.listen(process.env.PORT, () => {
+        console.log('connected!')
+    })
+})
+.catch((error) => {
+    console.log(error)
+})
+
+app.listen(process.env.PORT, () => {
     console.log('Listening on port 4000!')
 })
 
